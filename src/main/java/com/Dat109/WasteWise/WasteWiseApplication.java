2ca8159533d7f4b2ef15aeab524b7a3f5128a84d
olikve02 <oliver.kvebek@hotmail.com>
2024-04-17 13:09:06 +0200

    Dynamisk avfallpunkt på hjemmeside

--- src/main/java/com/Dat109/WasteWise/WasteWiseApplication.java
+++ src/main/java/com/Dat109/WasteWise/WasteWiseApplication.java
@@ -2,9 +2,16 @@ package com.Dat109.WasteWise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WasteWiseApplication {
+public class WasteWiseApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(WasteWiseApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WasteWiseApplication.class, args);
